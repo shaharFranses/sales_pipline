525c2cd0b604c25044bef1caa702765bb2eba56c
shaharFranses <shaharf4@gmail.com>
2025-12-23 13:13:33 +0200

    removed debugging logs and unnecessary comments

--- plugins/sales_pipeline_package/load_sales_pipeline.py
+++ plugins/sales_pipeline_package/load_sales_pipeline.py
@@ -12,7 +12,8 @@ def run_sales_elt():
     #create a db engine
     engine = sa.create_engine(DB_CONN_STRING)
     #creating the table since its a learning project, in production you would have this already created    
-    #creating raw table  for allt the data
+
+    #creating raw table  for all the data before data quality checks
     create_raw_sales_table_query = """
     CREATE TABLE IF NOT EXISTS raw_daily_sales (
     sale_id VARCHAR(50) PRIMARY KEY,
@@ -52,14 +53,13 @@ def run_sales_elt():
             connection.execute(sa.text(create_error_table))
             connection.execute(sa.text(create_sales_table_query))
 
+
+
     #first step E- extraction ,take data from csv and turn it into dataframe
     sales_df=pd.read_csv(SALES_FILE_PATH)
     sales_df = sales_df[['sale_id', 'transaction_date', 'product_category', 'amount', 'region']]
-    sales_df['amount'] = pd.to_numeric(sales_df['amount'], errors='coerce')
-    print(f"Columns found: {sales_df.columns.tolist()}")
-    print(f"Amount column type: {sales_df['amount'].dtype}")
-    ##data integrity checks
 
+    ##data integrity checks - crating a msking object to filter out bad rows
     bad_rows_mask = (
     (sales_df['sale_id'].isnull()) |
     (sales_df['transaction_date'].isnull()) |
@@ -74,14 +74,12 @@ def run_sales_elt():
 
     #second step L- load , take dataframe and load it into database ( 1000 rows per insert, raw daily sales will act as a staging table)
    
-   ##load good data
-   # sales_df.to_sql(name='raw_daily_sales',con=engine, if_exists='replace', index=False,chunksize=1000)
-   # DLQ_rows.to_sql(name='error_log',con=engine, if_exists='append', index=False,chunksize=1000)
-
-    ##load bad data to DLQ table
-
     sales_df.to_sql(name='raw_daily_sales',con=engine, if_exists='replace', index=False,chunksize=1000)
     DLQ_rows.to_sql(name='error_log',con=engine, if_exists='append', index=False,chunksize=1000)
+
+
+    #third step T- transform , take data from raw table and insert it into final table with upsert logic-
+    #updating old data and inserting new data
     sql_upsert_logic = """
     INSERT INTO daily_sales (sale_id, transaction_date, amount, region)
     SELECT sale_id, transaction_date, amount, region 
